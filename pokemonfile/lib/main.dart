6cec182ed63cf954c9ed4f28dbc1f02ff59fb14a
migueljnoboa <20190327@ce.pucmm.edu.do>
2023-11-05 22:53:43 -0400

    Cambios a la parte visual

--- pokemonfile/lib/main.dart
+++ pokemonfile/lib/main.dart
@@ -368,16 +368,34 @@ class PokemonCard extends StatelessWidget {
         child: Column(
           children: [
             Expanded(
-              flex: 2,
+              flex: 3,
               child: Container(
                 color: const Color.fromARGB(255, 222, 222, 222),
                 child: Stack(
                   children: [
+
+                    // Numero de Pokemon
+                    Container(
+                      alignment: Alignment.topLeft,
+                      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                      child: Text(
+                        '${int.parse(pokemon.url.split('/')[6])}',
+                        style: TextStyle(
+                          color: Colors.blueGrey,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+
+                    // Imagen de Pokeball en el bg
                     Center(
                       child: CachedNetworkImage(
                         imageUrl: 'https://github.com/Codeaamy/Pokedex/blob/master/images/pokeball.png?raw=true',
                       ),
                     ),
+
+                    // Imagen de Pokemon
                     Center(
                       child: CachedNetworkImage(
                         imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${int.parse(pokemon.url.split('/')[6])}.png',
@@ -386,12 +404,22 @@ class PokemonCard extends StatelessWidget {
                         fit: BoxFit.cover,
                       ),
                     ),
+
+                    // Icono de Favorito
+                    Container(
+                        alignment: Alignment.topRight,
+                        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                        child: Icon(
+                        Icons.favorite_border_outlined
+                        )
+                    ),
                   ],
                 ),
               ),
             ),
+
             Expanded(
-              flex: 1,
+              flex: 2,
               child: Container(
                 color: Colors.white,
                 child: Container(
@@ -399,38 +427,36 @@ class PokemonCard extends StatelessWidget {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
+
                       Text(
-                        '#${int.parse(pokemon.url.split('/')[6])}  ',
-                        style: const TextStyle(
-                          color: Colors.blueGrey,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),Text(
                         pokemon.name,
                         style: const TextStyle(
                           fontWeight: FontWeight.bold,
                         ),
                       ),
+
                       SizedBox(height: 5),
+
                       Row(
                         children: pokemon.pokemon?.types?.map((element) {
                           if (element != null) {
                             Color color = _getColorForElement(element.type.name);
                             Color textColor = textColorForBackground(color);
-                            return Padding(
-                              padding: EdgeInsets.only(right: 10),
+                            return Expanded(
                               child: Container(
-                                padding: EdgeInsets.all(8.0),
+                                margin: EdgeInsets.only(left: 3.0, right: 3.0),
+                                padding: EdgeInsets.only(top: 6.0, bottom: 6.0),
                                 decoration: BoxDecoration(
                                   color: color,
                                   borderRadius: BorderRadius.circular(8.0),
                                 ),
                                 child: Text(
                                   element.type.name,
-                                  style: TextStyle(fontSize: 10.0, color: textColor),
+                                  style: TextStyle(color: textColor),
+                                  textAlign: TextAlign.center,
                                 ),
                               ),
-                            );
+                          );
                           } else {
                             return SizedBox(); // Widget de reemplazo si el elemento es nulo
                           }
